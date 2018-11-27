ad2990e01d4702f2b99eade2d2ff90b31bbe6eb9
thatnewcarsmell <scalajr@gmail.com>
2018-11-27 16:48:09 -0700

    pruned index.js file of comments, edited knexfile.js to final deployed statusment

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     queries.listAll().then(students => res.send(students))
-    // res.status(200).send('I can still write basic routes.')
 })
 
-// app.use((err, req, res, next) => {
-//     const status = err.status || 500
-//     res.status(status).json({ error: err })
-// })
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err })
+})
 
 app.listen(port, () => {
     console.log(`There's something fishy going on over at port:${port}!!!!!!`)
