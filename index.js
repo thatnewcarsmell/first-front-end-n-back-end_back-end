2cd2a22dcad10eac21ce4a03a6c82ec2269ad34d
thatnewcarsmell <scalajr@gmail.com>
2018-11-27 16:35:55 -0700

    chasing bugs, is app.use not allowed?

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get('/', (req, res) => {
     // res.status(200).send('I can still write basic routes.')
 })
 
-app.use((err, req, res, next) => {
-    const status = err.status || 500
-    res.status(status).json({ error: err })
-})
+// app.use((err, req, res, next) => {
+//     const status = err.status || 500
+//     res.status(status).json({ error: err })
+// })
 
 app.listen(port, () => {
     console.log(`There's something fishy going on over at port:${port}!!!!!!`)
