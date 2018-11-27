03c056c59f2825250cb174df6a13238610801ec9
thatnewcarsmell <scalajr@gmail.com>
2018-11-27 09:34:21 -0700

    added response functionality, server is live

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const parser = require('body-parser')
+const port = process.env.PORT || 3000
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('I can still write basic routes.')
+})
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err })
+})
+
+app.listen(port, () => {
+    console.log(`There's something fishy going on over at port:${port}!!!!!!`)
+})
