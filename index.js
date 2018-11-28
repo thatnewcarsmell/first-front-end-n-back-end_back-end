ee767c54ed2420cd341215799969365cca47a11d
thatnewcarsmell <scalajr@gmail.com>
2018-11-28 09:47:26 -0700

    added a query and route to get student by id from the database

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 const parser = require('body-parser')
+const cors = require('cors')
 const queries = require('./queries')
 const port = process.env.PORT || 3000
 
+app.use(parser.json())
+// app.use(cors)
 
+app.param('id', (req,res,next,value) => {
+    id = parseInt(value)
+    // if(req.url.includes('series')){
+    //   object = seriesDB.find(series => series.id === id)
+    // } 
+    // else if(req.url.includes('characters')){
+    //   object = charactersDB.find(character => character.id === id)
+    // }
+    next()
+  })
+  
 
-app.get('/', (req, res) => {
+app.get('/students', (req, res) => {
     queries.listAll().then(students => res.send(students))
+})
+
+app.get('/student/:id', (req, res) => {
+    queries.getById(id).then(student => res.send(student))
 })
 
 app.use((err, req, res, next) => {
--- queries.js
+++ queries.js
@@ -3,5 +3,9 @@ const database = require('./database-connection')
 module.exports = {
     listAll(){
         return database('students')
+    },
+    getById(id){
+        return database('students')
+               .where('id', id)
     }
 }
